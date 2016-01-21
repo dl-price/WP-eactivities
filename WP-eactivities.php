9cf5a88573f63b1fe5a72fad7fabdc1e20e414b8
dlprice <daniel.price12@imperial.ac.uk>
2016-01-21 01:54:11 +0000

    Added plugin metadata

---
+++ WP-eactivities.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Eactivities
+ * Description: This plugin adds some features to connect to ICU eactivities and LDAP
+ * Version: 1.0.0
+ * Author: Daniel Price
+ * Author URI: http://dlprice.co.uk
+ * License: GPL2
+ */
